aa967f27ca8b8eadd34093abb4a5dbe5bba85625
YoungSeok Yoon <youngseok@cs.cmu.edu>
2015-01-06 10:37:18 -0500

    Range class is now immutable

--- edu.cmu.scs.fluorite/src/edu/cmu/scs/fluorite/commands/document/Range.java
+++ edu.cmu.scs.fluorite/src/edu/cmu/scs/fluorite/commands/document/Range.java
@@ -2,9 +2,16 @@ package edu.cmu.scs.fluorite.commands.document;
 
 import org.eclipse.jdt.core.ISourceRange;
 
+/**
+ * Represents a range in code.
+ * Works as an immutable object.
+ * 
+ * @author YoungSeok Yoon
+ *
+ */
 public class Range implements ISourceRange {
-	private int offset;
-	private int length;
+	private final int offset;
+	private final int length;
 	
 	public Range(int offset, int length) {
 		this.offset = offset;
@@ -25,5 +32,26 @@ public class Range implements ISourceRange {
 	
 	public static boolean overlap(Range r1, Range r2) {
 		return r1.getEndOffset() >= r2.getOffset() && r2.getEndOffset() >= r1.getOffset();
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + length;
+		result = prime * result + offset;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) { return true; }
+		if (obj == null) { return false; }
+		if (getClass() != obj.getClass()) { return false; }
+		
+		Range other = (Range) obj;
+		if (length != other.length) { return false; }
+		if (offset != other.offset) { return false; }
+		return true;
 	}
 }
